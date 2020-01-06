8a8235bf3a6f13edcf72e10b3a953fcd25a4b11f
4myc <missch3n@gmail.com>
2020-01-05 22:00:23 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// Remove the element DOM node 'main#main'
+document.querySelector('main#main').remove();
+// ALTERNATIVE:
+// main.remove()
+
+// Create a new element and assign it to the newHeader variable 
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.className = 'victory' //tests pass without this
+newHeader.innerHTML = "<p>Amy is the champion!</p>"
